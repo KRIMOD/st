1c2c8f087b5a029f3d0020849f81a6161850f5ce
Abdelkrim Temam <gm_temam@esi.com>
2021-06-11 19:06:53 +0100

    changed bg alpha value

--- config.h
+++ config.h
@@ -104,10 +104,10 @@ char *termname = "st-256color";
  *
  *	stty tabs
  */
-unsigned int tabspaces = 8;
+unsigned int tabspaces = 4;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.9;
 float alphaOffset = 0.0;
 float alphaUnfocus;
 
